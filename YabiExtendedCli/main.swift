091a843896a7a31fb6c299f11fa41d018cec4004
Nils Bergmann <nilsbergmann@noim.io>
2021-12-26 14:27:56 +0100

    feat: make this cli multi-threaded

--- YabiExtendedCli/main.swift
+++ YabiExtendedCli/main.swift
@@ -13,48 +13,102 @@ enum MyError: Error {
     case runtimeError(String)
 }
 
+let grabbedDispatchQueue = DispatchQueue(label: "grabbed-window");
+let focusDispatchQueue = DispatchQueue(label: "focus-queue");
+let currentDisplayQueue = DispatchQueue(label: "currentDisplay");
+let currentSpaceQueue = DispatchQueue(label: "currentDisplay");
+let lastSpaceQueue = DispatchQueue(label: "last-space");
+let firstSpaceQueue = DispatchQueue(label: "first-space");
+
+let grabbedIndexSemaphore = DispatchSemaphore(value: 0);
+let currentDisplaySemaphore = DispatchSemaphore(value: 0);
+let currentSpaceSemaphore = DispatchSemaphore(value: 0);
+let currentFocusedWindowSemaphore = DispatchSemaphore(value: 0);
+
+let lastSpaceSemaphore = DispatchSemaphore(value: 0);
+let firstSpaceSemaphore = DispatchSemaphore(value: 0);
+
+var targetSpaceIndex: Int = 0;
+
+var currentSpace: Int = 0;
+
+let event = CGEvent(source: nil)
+
+var grabbedIndex: Int?;
+
+var lastSpace: Int?;
+var firstSpace: Int?;
+
 do {
     guard let input = main.arguments.first else {
         throw MyError.runtimeError("Missing argument");
     }
     
-    var grabbedIndex: Int? = getGrabbedWindow();
+    currentSpaceQueue.async {
+        currentSpace = try! getCurrentSpaceForDisplay();
+        currentSpaceSemaphore.signal();
+    }
     
-    var targetSpaceIndex: Int = 0;
+    if input != "nextMoveWindow" && input != "backMoveWindow" {
+        grabbedDispatchQueue.async {
+            grabbedIndex = getGrabbedWindow();
+            currentSpaceSemaphore.wait();
+            grabbedIndexSemaphore.signal();
+        }
+    } else {
+        grabbedDispatchQueue.async {
+            var display: Int?;
+            currentDisplayQueue.async {
+                display = try! getCurrentDisplay();
+                currentDisplaySemaphore.signal();
+            }
+            var focusedWindow: Window?;
+            focusDispatchQueue.async {
+                focusedWindow = try! getFocusedWindow();
+                currentFocusedWindowSemaphore.signal();
+            }
+            currentDisplaySemaphore.wait();
+            currentSpaceSemaphore.wait();
+            currentFocusedWindowSemaphore.wait();
+            if focusedWindow!.display == display && focusedWindow!.space == currentSpace {
+                grabbedIndex = focusedWindow!.id;
+            }
+            grabbedIndexSemaphore.signal();
+        }
+    }
     
-    var currentSpace: Int = 0;
+    lastSpaceQueue.async {
+        lastSpace = try! getLastSpaceIndexForDisplay();
+        lastSpaceSemaphore.signal();
+    }
+    firstSpaceQueue.async {
+        firstSpace = try! getFirstSpaceIndexForDisplay();
+        firstSpaceSemaphore.signal();
+    }
     
-    let event = CGEvent(source: nil)
+    grabbedIndexSemaphore.wait();
     
     if input == "first" {
         targetSpaceIndex = try! getFirstSpaceIndexForDisplay();
     } else if input == "last" {
         targetSpaceIndex = try! getLastSpaceIndexForDisplay();
     } else {
-        currentSpace = try! getCurrentSpaceForDisplay();
-        
         if input == "next" || input == "nextMoveWindow"  {
-            let lastSpace = try! getLastSpaceIndexForDisplay();
-            if currentSpace == lastSpace {
-                targetSpaceIndex = try! getFirstSpaceIndexForDisplay();
+            lastSpaceSemaphore.wait();
+            if currentSpace == lastSpace! {
+                firstSpaceSemaphore.wait();
+                targetSpaceIndex = firstSpace!;
             } else {
                 targetSpaceIndex = currentSpace + 1;
             }
         } else if input == "back" || input == "backMoveWindow"  {
-            let firstSpace = try! getFirstSpaceIndexForDisplay();
-            if currentSpace == firstSpace {
-                targetSpaceIndex = try! getLastSpaceIndexForDisplay();
+            firstSpaceSemaphore.wait();
+            if currentSpace == firstSpace! {
+                lastSpaceSemaphore.wait();
+                targetSpaceIndex = lastSpace!;
             } else {
                 targetSpaceIndex = currentSpace - 1;
             }
-        }
-    }
-    
-    if input == "nextMoveWindow" || input == "backMoveWindow" {
-        let display = try! getCurrentDisplay();
-        let focusedWindow = try! getFocusedWindow();
-        if focusedWindow.display == display && focusedWindow.space == currentSpace {
-            grabbedIndex = focusedWindow.id;
         }
     }
     
@@ -62,8 +116,6 @@ do {
 
     if grabbedIndex != nil {
         moveWindowToSpace(windowIndex: grabbedIndex!, spaceIndex: targetSpaceIndex);
-        focusWindow(index: grabbedIndex!);
-        focusWindow(index: grabbedIndex!);
         focusWindow(index: grabbedIndex!);
     }
         
